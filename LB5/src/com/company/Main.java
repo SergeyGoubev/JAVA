d8509ee822d487293e35e3fb9826b78a61322b15
SergeyGoubev <62738328+SergeyGoubev@users.noreply.github.com>
2020-04-12 14:57:05 +0300

    Update Main.java

--- LB5/src/com/company/Main.java
+++ LB5/src/com/company/Main.java
@@ -1,6 +1,8 @@
 package com.company;
-import java.util.Scanner;
+        import java.util.Scanner;
 public class Main {
+
+    private static int ListResult;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
